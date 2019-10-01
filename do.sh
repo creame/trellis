9ba35c64d467b1d3543fe1fd3e80834a17229eaf
Paco Toledo <pacotole@crea.me>
2019-10-01 10:06:37 +0200

    colors in do.sh

--- do.sh
+++ do.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 shopt -s nullglob
 
+# shell colors
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+BLUE=`tput setaf 4`
+RESET=`tput sgr0`
+
 NUM_ARGS=2
 
 ENVIRONMENTS=( hosts/* )
@@ -45,11 +51,16 @@ fi
 HOSTS_FILE="hosts/$ENV"
 
 if [[ ! -e $HOSTS_FILE ]]; then
-  echo "Error: <$ENV> is not a valid environment ($HOSTS_FILE does not exist)."
+  echo
+  echo "${RED}Error: <$ENV> is not a valid environment ($HOSTS_FILE does not exist).${RESET}"
   echo
   echo "Available environments:"
   ( IFS=$'\n'; echo "${ENVIRONMENTS[*]}" )
+  echo
   exit 1
+else
+  echo "${BLUE}Doing $ACTION for '$SITE' <$ENV>...${RESET}"
+  echo
 fi
 
 if [ $ACTION == "provision" ]; then
@@ -57,31 +68,31 @@ if [ $ACTION == "provision" ]; then
 elif [ $ACTION == "deploy" ]; then
   ansible-playbook deploy.yml -e env=$ENV -e site=$SITE $EXTRA_PARAMS
 elif [ $ACTION == "uploads-push" ]; then
-  ansible-playbook uploads.yml -e env=$ENV -e site=$SITE -e mode=push
+  ansible-playbook uploads.yml  -e env=$ENV -e site=$SITE -e mode=push
 elif [ $ACTION == "uploads-pull" ]; then
-  ansible-playbook uploads.yml -e env=$ENV -e site=$SITE -e mode=pull
+  ansible-playbook uploads.yml  -e env=$ENV -e site=$SITE -e mode=pull
 elif [ $ACTION == "db-push" ]; then
   ansible-playbook database.yml -e env=$ENV -e site=$SITE -e mode=push
 elif [ $ACTION == "db-pull" ]; then
   ansible-playbook database.yml -e env=$ENV -e site=$SITE -e mode=pull
 elif [ $ACTION == "loco-pull" ]; then
-  ansible-playbook uploads.yml -e env=$ENV -e site=$SITE -e mode=loco
+  ansible-playbook uploads.yml  -e env=$ENV -e site=$SITE -e mode=loco
 elif [ $ACTION == "pull" ]; then
   ansible-playbook database.yml -e env=$ENV -e site=$SITE -e mode=pull
-  ansible-playbook uploads.yml -e env=$ENV -e site=$SITE -e mode=pull
-  ansible-playbook uploads.yml -e env=$ENV -e site=$SITE -e mode=loco
+  ansible-playbook uploads.yml  -e env=$ENV -e site=$SITE -e mode=pull
+  ansible-playbook uploads.yml  -e env=$ENV -e site=$SITE -e mode=loco
 elif [ $ACTION == "push" ]; then
   ansible-playbook database.yml -e env=$ENV -e site=$SITE -e mode=push
-  ansible-playbook uploads.yml -e env=$ENV -e site=$SITE -e mode=push
+  ansible-playbook uploads.yml  -e env=$ENV -e site=$SITE -e mode=push
 elif [ $ACTION == "ssh-web" ]; then
   ssh web@$(cat hosts/$ENV | sed -n 5p)
 elif [ $ACTION == "ssh-admin" ]; then
-  echo
+  echo $GREEN
   ansible-vault view group_vars/$ENV/vault.yml | grep "    password:"
-  echo
+  echo $RESET
   ssh admin@$(cat hosts/$ENV | sed -n 5p)
 else
-  echo "Error: <$ACTION> is not a valid action."
+  echo "${RED}Error: <$ACTION> is not a valid action.${RESET}"
   echo
   echo "Available actions:"
   echo "provision"
@@ -90,5 +101,6 @@ else
   echo "db-push / db-pull"
   echo "loco-pull"
   echo "ssh-web / ssh-admin"
+  echo
   exit 1
 fi
